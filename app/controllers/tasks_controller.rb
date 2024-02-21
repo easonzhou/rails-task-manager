e278b0f0c43f4421238ff028002192d43cdb7ec9
Yinsheng Zhou <zhouyinsheng@gmail.com>
2024-02-21 22:05:48 +0800

    edit is done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -17,7 +18,14 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+  end
+
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'tasks/new', to: "tasks#new", as: :new_task
   get 'tasks/:id', to: "tasks#show", as: :task
   post 'tasks', to: "tasks#create"
+  get 'tasks/:id/edit', to:'tasks#edit', as: :edit_task
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
