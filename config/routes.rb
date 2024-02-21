af5ce82ec05dbfe259468cd7bfec291482710c84
Yinsheng Zhou <zhouyinsheng@gmail.com>
2024-02-21 22:02:25 +0800

    create route is done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get '/', to: "tasks#index", as: :tasks
-  get '/tasks/new', to: "tasks#new", as: :new_task
-  get '/tasks/:id', to: "tasks#show", as: :task
+  get 'tasks', to: "tasks#index", as: :tasks
+  get 'tasks/new', to: "tasks#new", as: :new_task
+  get 'tasks/:id', to: "tasks#show", as: :task
+  post 'tasks', to: "tasks#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
