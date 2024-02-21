1e6c76686ee75a3804ed62d601d8b860ac6f3e86
Yinsheng Zhou <zhouyinsheng@gmail.com>
2024-02-21 22:14:27 +0800

    delete is working

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post 'tasks', to: "tasks#create"
   get 'tasks/:id/edit', to:'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
